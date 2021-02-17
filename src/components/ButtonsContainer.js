7d388cb30b16670519aec59da5c5f4ff68cbbba5
Kyd <info@kydkit.com>
2021-02-17 15:37:19 +0100

    Added ButtonContainer.js

---
+++ src/components/ButtonsContainer.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const ButtonsContainer = () => {
+  const [buttonValues, setButtonsVaules] = useState([
+      7,
+      8,
+      9,
+      "/",
+      4,
+      5,
+      6,
+      "x",
+      1,
+      2,
+      3,
+      "-",
+      "clear",
+      0,
+      "+",
+      "=",
+    ]);
+
+  return(
+    <div className={buttons}>
+      <p>some buttons here</p>
+    </div>
+  )
+}
+
+export default ButtonsContainer;
