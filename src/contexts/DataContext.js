cabcc1cf506e6627992ebe1bcd964bbc079c02e8
Oskar Anderberg <otto.anderberg@gmail.com>
2021-02-18 10:23:59 +0100

    Add calculator component to home page to troubleshoot

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -3,9 +3,28 @@ import { createContext, useState } from "react";
 export const DataContext = createContext();
 
 export default function DataProvider(props) {
+    const [dataInput, setDataInput] = useState("");
     
-    const values = {
+    const handleClick = (value) => {
+        if (/\d/.test(value)) {
+            setDataInput(dataInput + value);
+        } else if (value === "clear") {
+            setDataInput("");
+        } else if (value === "=") {
+            console.log("Calculating...");
+            calculate(dataInput);
+        } else {
+            setDataInput(dataInput + ` ${value} `);
+        }
+    };
 
+    const calculate = (calculation) => {
+        console.log(calculation);
+    }
+
+    const values = {
+        handleClick,
+        dataInput
     };
 
     return (
