8892028ca080fad1d76f5f61a0646584ace78b83
Hitomi <hitomi.winberg@gmail.com>
2021-02-18 09:46:39 +0100

    Added link home and about

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
+import {Link} from "react-router-dom"
 const Navbar = () => {
   return (
-    <div>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-      </ul>
-    </div>
+    <nav className="navbar">   
+       <Link to ="/">Home</Link>
+       <Link to ="/about">About</Link>     
+    </nav>
   );
 }
  
