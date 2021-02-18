6546638d699215e2839cac7fdfaac409da5a1392
Hitomi <hitomi.winberg@gmail.com>
2021-02-18 10:41:17 +0100

    Add Router to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
 import Home from "./pages/Home";
 import DataContext from "./contexts/DataContext";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
         <DataContext>
-        <Home />
+        <Route exact path="/"component={Home} />
       </DataContext>
+
+      </Router>
     </div>
   );
 }
