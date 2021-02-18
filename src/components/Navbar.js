c905458620bf943fab4378f6ec0f3a83664c05a6
Hitomi <hitomi.winberg@gmail.com>
2021-02-18 10:27:37 +0100

    Added Navbar style to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import {Link} from "react-router-dom"
+import {navbar} from "../css/Navbar.module.css"
 const Navbar = () => {
   return (
-    <nav className="navbar">   
+    <nav className={navbar}>   
        <Link to ="/">Home</Link>
        <Link to ="/about">About</Link>     
     </nav>
